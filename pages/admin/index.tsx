1a1886f7fb7399ca5b0c099a494c3034df4e90d0
GadaniPanth <gadanipanth@gmail.com>
2025-06-11 11:22:17 +0530

    new method for post data

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Navbar_admin from "@/components/Navbar_admin";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 export async function getStaticProps() {
   const blogRes = await fetch("https://dummyjson.com/posts?limit=250");
@@ -22,26 +22,49 @@ export default function Home() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  async function Submit() {
-    // console.log(email);
-    // console.log(password);
+  //   async function Submit() {
+  //     // console.log(email);
+  //     // console.log(password);
 
-    const body = new URLSearchParams();
-    body.append("email", email);
-    body.append("password", password);
+  //     const body = new URLSearchParams();
+  //     body.append("email", email);
+  //     body.append("password", password);
 
-    // const body = JSON.stringify({ email: email, password: password });
+  //     // const body = JSON.stringify({ email: email, password: password });
 
-    // console.log(body);
+  //     // console.log(body);
+
+  //     const res = await fetch(
+  //       "http://192.168.1.199/generatic-admin/api/admin_services/login/with_password",
+  //       {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/x-www-form-urlencoded",
+  //         },
+  //         body: body.toString(),
+  //       }
+  //     );
+
+  //     const resData = await res.json();
+
+  //     console.log(resData);
+  //   }
+
+  async function submit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+
+    console.log(formData);
+
+    for (const [key, value] of formData.entries()) {
+      console.log(key, value);
+    }
 
     const res = await fetch(
       "http://192.168.1.199/generatic-admin/api/admin_services/login/with_password",
       {
         method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: body.toString(),
+        body: formData,
       }
     );
 
@@ -60,22 +83,19 @@ export default function Home() {
       </Head>
       <Navbar_admin />
       <main className="wrapper">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            Submit();
-          }}
-        >
+        <form onSubmit={submit}>
           <input
             type="email"
             placeholder="Enter Your Email"
             value={email}
+            name="email"
             onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="password"
             placeholder="Enter Your Password"
             value={password}
+            name="password"
             onChange={(e) => setPassword(e.target.value)}
           />
 
